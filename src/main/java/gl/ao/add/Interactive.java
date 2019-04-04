c0acd0e7cbdb6fc95f5c5a16068b8b9b1e2f99d8
ao <andrew@hsmoore.com>
2019-04-04 13:45:23 +0100

    Update UI

--- src/main/java/gl/ao/add/Interactive.java
+++ src/main/java/gl/ao/add/Interactive.java
@@ -1,5 +1,9 @@
 package gl.ao.add;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Interactive {
 
     /***
@@ -8,73 +12,8 @@ public class Interactive {
      * @param uri
      * @return
      */
-    public static String IndexTemplate(String host, String uri) {
-        return "<html>\n" +
-                "   <head>\n" +
-                "       <title>Interactive</title>\n" +
-                "       <script type='text/javascript'>\n" +
-                "           var _submitQuery = function() {\n" +
-                "               var xhr = new XMLHttpRequest();\n" +
-                "               xhr.open('POST', '"+host+"/post?query', true);\n" +
-                "               xhr.setRequestHeader('Content-Type', 'application/json');\n" +
-                "               xhr.onreadystatechange = function () {\n" +
-                "                   if (this.readyState != 4) return;\n" +
-                "                   if (this.status == 200) {\n" +
-                "                       var data = '';\n" +
-                "                       try {\n" +
-                "                           data = JSON.parse(this.responseText);\n" +
-                "                           htmlData = JSON.stringify(data);\n" +
-                "                       } catch (ex) {\n" +
-                "                           data = this.responseText;\n" +
-                "                           htmlData = data;\n" +
-                "                       }\n" +
-                "                       console.log(data);\n" +
-                "                       var all = document.getElementById('responseContainer').innerHTML;\n" +
-                "                       document.getElementById('responseContainer').innerHTML = htmlData+'<br/>'+all;\n" +
-                "                   }\n" +
-                "               };\n" +
-                "               xhr.send(JSON.stringify({\n" +
-                "                   value: document.getElementById('txtQuery').value\n" +
-                "               }));\n" +
-                "           };\n" +
-                "           function keyUpQuery(event) {\n" +
-                "               if (event.keyCode == 13) {\n" +
-                "                   var content = this.value;  \n" +
-                "                   if(event.shiftKey){\n" +
-                "                       _submitQuery();\n" +
-                "                       event.stopPropagation();\n" +
-                "                   }\n" +
-                "               }\n" +
-                "           }\n" +
-                "           function populate(el) {\n" +
-                "               document.getElementById('txtQuery').value = el.innerHTML;\n" +
-                "           }\n" +
-                "       </script>\n" +
-                "       <style>\n" +
-                "           #txtQuery {\n" +
-                "               margin: 0px;\n" +
-                "               width: 100%;\n" +
-                "               min-height: 200px;\n" +
-                "           }\n" +
-                "           .populate {\n" +
-                "               cursor:pointer;\n" +
-                "           }\n" +
-                "       </style>\n" +
-                "   </head>\n" +
-                "   <body>\n" +
-                "       <h1>Interactive!</h1>\n" +
-                "       <div>\n" +
-                "           <div class='populate' onclick='populate(this)'>create database db1</div>\n" +
-                "           <div class='populate' onclick='populate(this)'>show databases</div>\n" +
-                "           <div class='populate' onclick='populate(this)'>create table db1.users</div>\n" +
-                "           <div class='populate' onclick='populate(this)'>show db1 tables</div>\n" +
-                "           <div class='populate' onclick='populate(this)'>insert into db1.users ('name') values('andrew')</div>\n" +
-                "           <div class='populate' onclick='populate(this)'>new uuid test</div>\n" +
-                "       </div>\n" +
-                "       <div><textarea id='txtQuery' onkeyup='keyUpQuery(event)'></textarea></div>\n" +
-                "       <div><input type='button' value='Submit query' onclick='_submitQuery()' /></div>\n" +
-                "       <div id='responseContainer'></div>\n" +
-                "   </body>\n" +
-                "</html>";
+    public static String IndexTemplate(String host, String uri) throws IOException {
+        String htmlString = new String(Files.readAllBytes(Paths.get(Construct.res_path+"interactive.html")), "UTF-8");
+        return htmlString;
     }
 }
