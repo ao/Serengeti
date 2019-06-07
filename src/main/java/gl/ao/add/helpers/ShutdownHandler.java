8e05244b2b95a0ab65df2c777f7c52c433c155f6
ao <andrew@hsmoore.com>
2019-06-07 19:35:28 +0100

    ShutdownHandler not working on Windows 10 notice

--- src/main/java/gl/ao/add/helpers/ShutdownHandler.java
+++ src/main/java/gl/ao/add/helpers/ShutdownHandler.java
@@ -5,6 +5,14 @@ import gl.ao.add.ADD;
 public class ShutdownHandler {
 
     public ShutdownHandler() {
+
+        /**
+         * This does not appear to work correctly on Windows 10 during testing
+         * There are bugs online stating the same discovery regarding Windows
+         * e.g. https://netbeans.org/bugzilla/show_bug.cgi?id=22641
+         * e.g. https://bugs.openjdk.java.net/browse/JDK-7068835
+         */
+
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             @Override
             public void run() {
