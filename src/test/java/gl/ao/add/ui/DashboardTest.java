c96b1459f6e39fe4d164a8800c012deafee159e5
ao <andrew@hsmoore.com>
2019-06-26 10:13:08 +0100

    Larger artwork piece

--- src/test/java/gl/ao/add/ui/DashboardTest.java
+++ src/test/java/gl/ao/add/ui/DashboardTest.java
@@ -13,7 +13,8 @@ class DashboardTest {
 
     @Test
     void indexTemplate() throws IOException {
-        String interactive = new String(Files.readAllBytes(Paths.get(Globals.res_path + "dashboard.html")))+"1";
+        String interactive = new String(Files.readAllBytes(Paths.get(Globals.res_path + "dashboard.html")));
         assertEquals(Dashboard.IndexTemplate("test", "test"), interactive);
     }
 }
+
